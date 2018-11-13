ebc117c87c0dec0032b62fcf1f987d8999a6308b
liaokai <870306515@qq.com>
2018-11-13 14:36:21 +0800

    all commit

--- src/main/java/com/liao/BlogApiApplication.java
+++ src/main/java/com/liao/BlogApiApplication.java
@@ -2,9 +2,18 @@ package com.liao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogApiApplication {
+@ServletComponentScan
+public class BlogApiApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BlogApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(BlogApiApplication.class);
